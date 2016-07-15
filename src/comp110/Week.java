01a90fae88fecceb569bf14228aa3c53ed22fe1c
Kris Jordan <krisjordan@gmail.com>
2016-07-15 10:25:22 -0400

    Update week.

--- src/comp110/Week.java
+++ src/comp110/Week.java
@@ -2,15 +2,25 @@ package comp110;
 
 public class Week {
 
+  private String _title;
   private Shift[][] _shifts;
 
   public Week() {
+    _title = "";
     _shifts = new Shift[7][24];
     for (int day = 0; day < _shifts.length; day++) {
       for (int hour = 0; hour < _shifts[day].length; hour++) {
         _shifts[day][hour] = new Shift(hour, 0);
       }
     }
+  }
+
+  public String getTitle() {
+    return _title;
+  }
+
+  public void setTitle(String title) {
+    _title = title;
   }
 
   public Shift[][] getShifts() {
@@ -45,6 +55,10 @@ public class Week {
 
   public String toString() {
     StringBuilder sb = new StringBuilder();
+
+    sb.append("** ");
+    sb.append(_title);
+    sb.append(" **");
 
     for (int day = 0; day < _shifts.length; day++) {
       sb.append("Day: " + day + "\n");

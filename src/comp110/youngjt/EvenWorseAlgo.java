31cd3e9f8a7007327d2e746befcb6d3f9cc19d6b
Jeffrey <youngjt@live.unc.edu>
2016-07-16 22:29:33 -0400

    more work on algo, added some experience logic

--- src/comp110/youngjt/EvenWorseAlgo.java
+++ src/comp110/youngjt/EvenWorseAlgo.java
@@ -162,13 +162,87 @@ public class EvenWorseAlgo implements SchedulingAlgo {
         }
       }
     }
+    
+    /* POST-PROCESS - ensure that we have skill level > 1.5 */
+    for (int i = 0; i < 10; i++) {
+      // loop over each shift
+      for (int day = 0; day < shifts.length; day++) {
+        for (int hour = 0; hour < shifts[0].length; hour++) {
+          // getting all employees scheduled during a particular shift
+          ArrayList<Employee> employeesScheduledDuringShift = new ArrayList<Employee>();
+          Shift shift = shifts[day][hour];
+          for (Employee e : shift) {
+            employeesScheduledDuringShift.add(e);
+          }
+          // check average skill
+          if (getAverageSkill(employeesScheduledDuringShift) < 1.5) {
+            Employee lowSkill = findLowestSkillEmployee(employeesScheduledDuringShift);
+            //if nobody was scheduled previously for this shift
+            if (lowSkill.getName().equals("dummy")){
+              break;
+            }
+            shift.remove(lowSkill);
+            //find someone higher skill
+            boolean foundReplacement = false;
+            for (Employee newEmployee : employees){
+              if (newEmployee.isAvailable(day, hour) && newEmployee.getCapacityRemaining() > 0
+                  && !shift.contains(newEmployee) && newEmployee.getLevel() >= 2  && hasSomeoneScheduledOfOppositeGender(newEmployee, shift)){
+                shift.add(newEmployee);
+                //scheduling them contiguously would also be nice
+                scheduleContiguouslyPostProcess(newEmployee, day, hour, shifts, random);
+                foundReplacement = true;
+              }
+            }
+            //if we cannot find suitable replacement then just re-add old person
+            if (!foundReplacement){
+              shift.add(lowSkill);
+            }
+            //if we were able to replace find another shift that person was scheduled for and try to schedule them contiguosuly from there
+            else {
+              for (int day2 = 0; day2 < shifts.length; day2++){
+                for (int hour2 = 0; hour2 < shifts[0].length; hour2++){
+                  ArrayList<Employee> employeesScheduledDuringShiftReschedule = new ArrayList<Employee>();
+                  Shift rescheduledShift = shifts[day2][hour2];
+                  for (Employee e : rescheduledShift) {
+                    employeesScheduledDuringShiftReschedule.add(e);
+                  }
+                  //we need to check capacity as well since to prevent this if from executing multiple times
+                  if (!rescheduledShift.contains(lowSkill) && lowSkill.getCapacity() > 0 && getAverageSkill(employeesScheduledDuringShiftReschedule) > 1.5 && hasSomeoneScheduledOfOppositeGender(lowSkill, rescheduledShift)){
+                    rescheduledShift.add(lowSkill);
+                    scheduleContiguouslyPostProcess(lowSkill, day, hour, shifts, random);
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+
 
     return input;
   }
   
-  //identical to other contiguous method except we are a little more relaxed on requirements needed to schedule (no skill or gender check)
-  private void scheduleContiguouslyPostProcess(Employee employee, int startDay, int startHour,
+  //basically finding min
+  private Employee
+      findLowestSkillEmployee(ArrayList<Employee> employees) {
+    if (employees.size() == 0){
+       return new Employee("dummy", 0, false, -1, null);
+    }
+    Employee min = employees.get(0);
+    for (Employee e : employees) {
+      if (e.getLevel() < min.getLevel()) {
+        ///new min found
+        min = e;
+      }
+    }
+    return min;
+  }
+
+  //identical to other contiguous method except we are a little more relaxed on requirements needed to schedule (no skill check)
+  private boolean scheduleContiguouslyPostProcess(Employee employee, int startDay, int startHour,
       Shift[][] shifts, Random rand) {
+    boolean successful = false;
     int numberOfHoursToSchedule = Math.min((23 - startHour) , generateRandomInt(1, 3, rand));
     
     for (int i = 0; i < numberOfHoursToSchedule; i++){
@@ -180,10 +254,12 @@ public class EvenWorseAlgo implements SchedulingAlgo {
       }
 
       if(employee.isAvailable(startDay, startHour + i + 1) && employee.getCapacityRemaining() > 0
-          && !currentShift.contains(employee)){
+          && !currentShift.contains(employee) && hasSomeoneScheduledOfOppositeGender(employee, currentShift)){
         currentShift.add(employee);
+        successful = true;
       }
     }
+    return successful;
   }
 
   private double getAverageSkill(ArrayList<Employee> employees){

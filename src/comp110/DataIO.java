b7211e92925a7e950946dce14f1bc83fff54aff3
Jeffrey-Young <youngjt@live.unc.edu>
2016-09-20 20:33:21 -0400

    back to working state

--- src/comp110/DataIO.java
+++ src/comp110/DataIO.java
@@ -26,14 +26,23 @@ public class DataIO {
   static Staff parseStaff(String dir) throws IOException {
     Staff staff = new Staff();
     File csvDirectory = new File(dir);
-    BufferedReader csvReader;
+    BufferedReader csvReader = null;
     for (File csv : csvDirectory.listFiles()) {
-      csvReader = new BufferedReader(new FileReader(csv));
-      // parsing employee info
-      String name = csvReader.readLine().split(",")[1]; // ugly
-      String gender = csvReader.readLine().split(",")[1];
-      int capacity = Integer.parseInt(csvReader.readLine().split(",")[1]);
-      int level = Integer.parseInt(csvReader.readLine().split(",")[1]);
+      String name = "";
+      String gender = "";
+      int capacity = 0;
+      int level = 0;
+      try {
+        csvReader = new BufferedReader(new FileReader(csv));
+        // parsing employee info
+        name = csvReader.readLine().split(",")[1]; // ugly
+        gender = csvReader.readLine().split(",")[1];
+        capacity = Integer.parseInt(csvReader.readLine().split(",")[1]);
+        level = Integer.parseInt(csvReader.readLine().split(",")[1]);
+      } catch (NullPointerException e) {
+        System.err.println("Error parsing: " + name);
+        e.printStackTrace();
+      }
 
       csvReader.readLine(); // throw away header line with days
 
@@ -43,7 +52,12 @@ public class DataIO {
         String scheduleLine = csvReader.readLine();
         for (int day = 0; day < 7; day++) {
           // Offset by 1 accounts for label in CSV
-          availability[day][hour] = Integer.parseInt(scheduleLine.split(",")[day + 1]);
+          try {
+            availability[day][hour] = Integer.parseInt(scheduleLine.split(",")[day + 1]);
+          } catch (NumberFormatException e) {
+            System.err.println("Error:" + name);
+            e.printStackTrace();
+          }
         }
       }
       staff.add(new Employee(name, capacity, gender.equals("M") ? false : true, level, availability));

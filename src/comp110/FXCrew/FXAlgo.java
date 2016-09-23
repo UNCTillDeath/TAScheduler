2c210ec3135b511bd4a37d75da03d2119c1ba276
Jeffrey <youngjt@live.unc.edu>
2016-09-23 00:45:44 -0400

    new schedules generated

--- src/comp110/FXCrew/FXAlgo.java
+++ src/comp110/FXCrew/FXAlgo.java
@@ -386,7 +386,7 @@ private boolean possibleHasGenderBalance(Employee toAdd, Employee toRemove, Shif
 
     KarenBot karenBot = new KarenBot(new FXAlgo());
 
-    karenBot.run("week1", 100);
+    karenBot.run("week1", 10000);
 
   }
 

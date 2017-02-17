2c0af69076e6308ac47374ab6d6fbeba3066650b
djsteffey <djsteffey@users.noreply.github.com>
2017-02-17 10:40:47 -0500

    Update Storage.java

--- src/comp110/Storage.java
+++ src/comp110/Storage.java
@@ -1,6 +1,11 @@
 package comp110;
 
 public class Storage{
+  public enum EStorageResult { SUCCESS, FAILED_INVALID_USERNAME_PASSWORD, FAILED_CONNECTION_TIMEOUT, FAILED_REPO_NOT_FOUND, FAILED_MERGE_FAILED };
+  
+  public Storage(){
+  }
+  
   public void pullFiles(){
 
   }

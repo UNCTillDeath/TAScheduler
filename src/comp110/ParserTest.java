d62200f3f85b02ac9fc1dd2d0af6365b16589f0d
Keith Whitley <kewhitle@live.unc.edu>
2017-03-25 13:27:05 -0400

    created Junit test file for Parser

---
+++ src/comp110/ParserTest.java
@@ -0,0 +1,25 @@
+package comp110;
+
+import static org.junit.Assert.*;
+import java.io.File;
+import org.junit.*;
+
+public class ParserTest{
+    @Test
+    public void parseEmployee_test(){
+        Parser p = new Parser();
+        
+    }
+    @Test
+    public void parseSchedule_test(){
+
+    }
+    @Test
+    public void writeFile_test(){
+
+    }
+    @Test 
+    public void writeScheduleToJson_Test(){
+
+    }
+}
